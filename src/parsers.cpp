a38eccbfa9b4b440b6773c883b1a1ae44f4954f7
Ryan Furrer <rfurrer@ualberta.ca>
2018-11-30 21:48:43 -0700

    Looks quite stubby

--- src/parsers.cpp
+++ src/parsers.cpp
@@ -7,3 +7,25 @@ int validateArgs(int argc, char *argv[]) {
 
     return 0;
 }
+
+CLI_ARGS parseArgs(int argc, char *argv[]) {
+    CLI_ARGS args;
+    args.inputFile = argv[1];
+    args.monitorTime = (atoi(argv[2]));
+    args.iterations = (atoi(argv[3]));
+    return args;
+}
+
+uint parseTaskID(const string &taskIDString) {
+
+}
+
+LINE_TYPES getInputFileType(const string &line);
+
+FRAME parseInputFileLine(const string &line);
+
+ResourceArg parseResourceArg(const string &arg);
+
+ResourcesLine parseResourcesLine(const string &line);
+
+TaskLine parseTaskLine(const string &line);
